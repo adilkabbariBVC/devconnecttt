b0eb319deecdae9799254ee409bb11953bd634cc
Adil Kabbari <a.kabbari692@mybvc.ca>
2025-06-14 18:27:39 -0600

    delted unused code

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.73:3333', // IP address
+  baseURL: 'http://192.168.1.73:3333', // replace with your IP address
 });
 
 export default api;
